8a5fd5de10eb1a235e437b4e49379de14a600a37
Kim Seunghwan <61crossroad@gmail.com>
2026-04-21 03:39:07 +0900

    post Htmx request

--- src/main/java/org/tezz/htmx_demo/HomeController.java
+++ src/main/java/org/tezz/htmx_demo/HomeController.java
@@ -1,12 +1,22 @@
 package org.tezz.htmx_demo;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @PostMapping("/clicked")
+    public String postMethodName(Model model) {
+        model.addAttribute("now", LocalDateTime.now().toString());
+        return "clicked :: result";
     }
 }
