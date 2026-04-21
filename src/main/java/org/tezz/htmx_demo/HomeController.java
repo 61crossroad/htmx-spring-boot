31311b0d0dbb00e2640a2fadba50068d840f584f
Kim Seunghwan <61crossroad@gmail.com>
2026-04-21 17:55:58 +0900

    Htmx-powered Search

--- src/main/java/org/tezz/htmx_demo/HomeController.java
+++ src/main/java/org/tezz/htmx_demo/HomeController.java
@@ -1,6 +1,7 @@
 package org.tezz.htmx_demo;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,25 @@ import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
+
+    static List<String> searchResults = List.of("one", "two", "three", "four", "five");
+
     @GetMapping("/")
     public String home() {
         return "index";
     }
+
+    @GetMapping("/search")
+    public String getMethodName(String q, Model model) {
+        var filtered = searchResults
+            .stream()
+            .filter(s -> s.startsWith(q.toLowerCase()))
+            .toList();
+
+        model.addAttribute("results", filtered);
+        return "search :: results";
+    }
+    
 
     @PostMapping("/clicked")
     public String postMethodName(Model model) {
